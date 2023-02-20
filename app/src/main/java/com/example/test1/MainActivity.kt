0f7842aae8844e73410b948e801e7f455146080c
Bahaa Elramlawi <b.elramlawi300@gmail.com>
2023-02-20 14:40:00 +0200

    Hello world

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -34,6 +34,11 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun Hello(name: String) {
+    Text(text = "Hello $name!")
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
